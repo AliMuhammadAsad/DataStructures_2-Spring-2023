ec9ac83dc7db0f97dc6f5ceeca35201e1cb723e5
Ali Muhammad Asad <aa07190@st.habib.edu.pk>
2023-01-22 16:39:41 +0500

    Counter implementation of Stack

---
+++ Stack/stack_counter.py
@@ -0,0 +1,9 @@
+## This file contains an implementation of a stack in python through the use of a counter variable inside the class ##
+
+## The counter variable will be used to keep track of the elements in the stack - ie the head of the stack ##
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+        counter = 0
+    
