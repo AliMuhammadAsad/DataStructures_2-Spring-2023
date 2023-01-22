5cd4a8fa3cc362b8f345d8045b78c9560894847f
Ali Muhammad Asad <aa07190@st.habib.edu.pk>
2023-01-22 17:26:49 +0500

    Simple Queue Class

---
+++ Queue/simple_queue.py
@@ -0,0 +1,9 @@
+## This file contains a simple implementation of Queue using lists in python - python does not have arrays so lists can be used as arrays ##
+## Implementation done using classes ##
+
+class Queue:
+    def __init__(self)->None:
+        self.queue = []
+    
+
+    
